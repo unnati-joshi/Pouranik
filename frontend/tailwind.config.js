0fead77b3eeb6d1259ccc73649a86d6c13d2c61a
oneal <khalifaanil84@gmail.com>
2025-07-16 08:21:06 +0545

    refactor: streamline Tailwind CSS configuration by using imports for plugins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import aspect from '@tailwindcss/aspect-ratio';
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    forms,
+    typography,
+    aspect,
   ],
 };
