faa8b33e48d37964cc8f706345c497f294f97efc
ArihantSrivastava2225/ <arihantsrivastava71011@gmail.com>
2025-07-25 14:45:20 +0530

    Implemented Shelves and AllBooks view in library

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -3,11 +3,13 @@ import { IoLibraryOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 import ShelfSection from '../components/Library_components/ShelfSection';
 import useLibraryBooks from "../components/Library_components/books";
+import AllBooksSection from "../components/Library_components/AllBooksSection";
 
 const Library = () => {
     const [_, setToken] = useState(localStorage.getItem("token"));
     const [reloadTrigger, setReloadTrigger] = useState(false);
-    
+    const [libView, setLibView] = useState("shelves"); // "shelves" or "allBooks"
+
     //for library book data to be refetched for different users as they sign in i.e. as token gets changed, its like refreshing
     useEffect(() => {
         const handleStorage = () => {
@@ -22,10 +24,9 @@ const Library = () => {
     const { currentlyReading, nextUp, finished } = useLibraryBooks(reloadTrigger);
 
     const onBookChange = () => {
-  setReloadTrigger(prev => !prev); // toggles to trigger useEffect
-};
+        setReloadTrigger(prev => !prev); // toggles to trigger useEffect
+    };
 
-    
     return (
         <div className='flex flex-col justify-center items-center !m-7'>
             <section className='w-[80%] space-y-2 mt-7'>
@@ -42,23 +43,29 @@ const Library = () => {
 
             <section className='w-[94%] bg-orange-50 rounded-2xl !p-4'>
                 <div className='flex justify-between items-center bg-white/50 rounded-2xl'>
-                  <div>
-                    <button className='!text-[#9ca3af] hover:!text-[#a16207] !bg-white hover:!bg-[#f5e8dc]'>Shelves</button>
-                  <button className='!text-[#9ca3af] hover:!text-[#a16207] !bg-white hover:!bg-[#f5e8dc]'>All Books</button>
-                  </div>
-                  <div className='flex justify-center items-center'>
-                    <CiSearch />
-                    <input type="text" />
-                  </div>
+                    <div>
+                        <button className='!text-[#9ca3af] hover:!text-[#a16207] !bg-white hover:!bg-[#f5e8dc]' onClick={() => setLibView("shelves")}>Shelves</button>
+                        <button className='!text-[#9ca3af] hover:!text-[#a16207] !bg-white hover:!bg-[#f5e8dc]' onClick={() => setLibView("allBooks")}>All Books</button>
+                    </div>
+                    <div className='flex justify-center items-center'>
+                        <CiSearch />
+                        <input type="text" />
+                    </div>
                 </div>
                 <section className='w-[94%] h-[800px] grid grid-cols-1 grid-rows-3 gap-4'>
-                <ShelfSection title="Currently reading" books={currentlyReading} onBookChange={onBookChange} />
-                <ShelfSection title="Next up" books={nextUp} onBookChange={onBookChange} />
-                <ShelfSection title="Finished" books={finished} onBookChange={onBookChange} />
-            </section>
+                    {libView==="shelves" ? (
+                        <>
+                          <ShelfSection title="Currently reading" books={currentlyReading} onBookChange={onBookChange} />
+                    <ShelfSection title="Next up" books={nextUp} onBookChange={onBookChange} />
+                    <ShelfSection title="Finished" books={finished} onBookChange={onBookChange} />
+                        </>
+                    ): (
+                        <AllBooksSection title="All Books" books={[...currentlyReading, ...nextUp, ...finished]} onBookChange={onBookChange} />
+                    )}
+                </section>
             </section>
         </div>
     )
 }
 
-export default Library
+export default Library;
